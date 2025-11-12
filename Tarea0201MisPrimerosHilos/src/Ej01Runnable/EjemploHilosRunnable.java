fab3756b93971f155a1658098266fee5ee52c265
hebrabo <hebrabo@gmail.com>
2025-11-12 08:37:34 +0100

    Actividad corregida. Refactor EjemploHilosRunnable: sustituido código repetitivo por bucle y array para crear y gestionar los 8 hilos; mensajes del hilo principal unificados;

--- Tarea0201MisPrimerosHilos/src/Ej01Runnable/EjemploHilosRunnable.java
+++ Tarea0201MisPrimerosHilos/src/Ej01Runnable/EjemploHilosRunnable.java
@@ -3,60 +3,33 @@ package Ej01Runnable;
 public class EjemploHilosRunnable {
     public static void main(String[] args) {
 
+        int NUM_HILOS = 8;
+        Thread[] hilos = new Thread[NUM_HILOS];
+
         // Creamos 8 instancias de la clase MiTareaRunnable
-        Runnable tarea1 = new MiTareaRunnable();
-        Runnable tarea2 = new MiTareaRunnable();
-        Runnable tarea3 = new MiTareaRunnable();
-        Runnable tarea4 = new MiTareaRunnable();
-        Runnable tarea5 = new MiTareaRunnable();
-        Runnable tarea6 = new MiTareaRunnable();
-        Runnable tarea7 = new MiTareaRunnable();
-        Runnable tarea8 = new MiTareaRunnable();
+        for (int i=0; i < NUM_HILOS; i++) {
+            hilos[i] = new Thread(new MiTareaRunnable());
+            hilos[i].start();
+        }
 
-        // Creamos 8 objetos Thread y les pasamos las instancias Runnable
-        Thread hilo1 = new Thread(tarea1);
-        Thread hilo2 = new Thread(tarea2);
-        Thread hilo3 = new Thread(tarea3);
-        Thread hilo4 = new Thread(tarea4);
-        Thread hilo5 = new Thread(tarea5);
-        Thread hilo6 = new Thread(tarea6);
-        Thread hilo7 = new Thread(tarea7);
-        Thread hilo8 = new Thread(tarea8);
-
-        // Iniciamos los hilos
-        hilo1.start();
-        hilo2.start();
-        hilo3.start();
-        hilo4.start();
-        hilo5.start();
-        hilo6.start();
-        hilo7.start();
-        hilo8.start();
-
-        // El hilo principal (main) también realiza algunas tareas
-        for (int i = 1; i<= 5; i++){
-            System.out.println("Hilo principal: Contador " + i);
-            try {
+        for (int i=1; i<=5; i++){
+            System.out.println("Soy el hilo principal y ejecuto el contador " + i);
+            try{
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
         }
 
-        try {
-            // Esperamos a que todos los hilos terminen
-            hilo1.join();
-            hilo2.join();
-            hilo3.join();
-            hilo4.join();
-            hilo5.join();
-            hilo6.join();
-            hilo7.join();
-            hilo8.join();
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (Thread h: hilos) {
+            try {
+                h.join();
+            } catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
         }
-        System.out.println("Programa finalizado");
+
+        System.out.println("FIN DEL PROGRAMA");
+
     }
 }
--- Tarea0201MisPrimerosHilos/src/Ej01Runnable/MiTareaRunnable.java
+++ Tarea0201MisPrimerosHilos/src/Ej01Runnable/MiTareaRunnable.java
@@ -19,11 +19,13 @@ public class MiTareaRunnable implements Runnable{
     @Override
     public void run() {
         for (int i=1; i<=5; i++){
-            System.out.println("Hilo" + Thread.currentThread().getId() + " : Contador " + i);
+            // get.Id() deprecated
+            // System.out.println("Hilo" + Thread.currentThread().getId() + " : Contador " + i);
+            System.out.println("Soy el hilo " + Thread.currentThread().threadId() + " y ejecuto el contador " + i);
             try{
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
         }
     }
