471905466c028f90c7a62ed338950b65dc102d66
hebrabo <hebrabo@gmail.com>
2025-11-20 14:24:05 +0100

    Eliminar archivos innecesarios

--- Tarea0203PrioridadEnHilos/src/PrioridadRunnable.java
+++
@@ -1,9 +0,0 @@
-public class PrioridadRunnable implements Runnable {
-    @Override
-    public void run () {
-        System.out.println("Hilo " + Thread.currentThread().getName() + " con prioridad " + Thread.currentThread().getPriority());
-    }
-}
-
-
-
